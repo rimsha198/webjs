52a42edbacab1f396c2964c3f0480e116babbaba
rimsha198 <rimshaasgharr24@gmail.com>
2024-08-21 11:12:47 +0500

    basic

---
+++ table arr.js/table.js
@@ -0,0 +1,14 @@
+
+
+
+var arr=[
+    { name : "rimsha",age:20,color:"red"},
+    { name : "shabana",age:20,color:"red"},
+    { name : "areej",age:20,color:"red"},
+    { name : "saman",age:20,color:"red"}
+]
+
+
+for (i=0;i<=arr.length;i++){
+    console.log(i)
+}
